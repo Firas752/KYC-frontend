ed84518bf22db38b2aa420c76ee41416b2d139bf
yhoungdev <yhoungdev@gmail.com>
2024-01-03 22:55:11 +0100

    chore: update

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -6,6 +6,7 @@ const Page = () => {
 	return (
 		//@ts-ignore
 		<DashboardLayout title="Applications" isBack={true}>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat exercitationem autem sed sunt doloribus id nisi soluta nam. Eligendi numquam magnam provident eius aliquam deserunt veritatis dolore voluptate, doloremque nulla.
 			<Applications />
 		</DashboardLayout>
 	);
